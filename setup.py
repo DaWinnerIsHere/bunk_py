456f50bbef9757dbc6fcf8cf828563dabab00f8d
DaWinnerIsHere <71647641+DaWinnerIsHere@users.noreply.github.com>
2021-10-04 03:14:24 -0500

    a
    a

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='bunk_py',
+   version='1.0',
+   description='An API wrapper for BunkServices API.',
+   author='DaWinnerIsHere',
+   author_email='dawinnerishere@gmail.com',
+   packages=['bunk_py'],
+   install_requires=['wheel', 'bar', 'greek'],
+)
